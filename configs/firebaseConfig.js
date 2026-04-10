a6989011f69bb3e7457a5caf7de17abab4878160
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2026-04-10 15:17:49 +0530

    ADD : Integrated firebase SDK in react native

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmmuE5AFb8GOC4daMLa1T9hqbwX45nJB0",
+  authDomain: "task-application-71b03.firebaseapp.com",
+  projectId: "task-application-71b03",
+  storageBucket: "task-application-71b03.firebasestorage.app",
+  messagingSenderId: "157326903951",
+  appId: "1:157326903951:web:396027d7844e173219befa",
+  measurementId: "G-W2W5HJ0SQK",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
